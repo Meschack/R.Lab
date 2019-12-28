760b47cca543178bab2393d80c85855569e3840e
Irung <47919550+Meschack@users.noreply.github.com>
2019-12-28 21:42:24 +0200

    Add files via upload

---
+++ R.Lab/js/main.js
@@ -0,0 +1,24 @@
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<3;i++) {
+    next=next.next();
+    if (!next.length) {
+      next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
+wow = new WOW({
+  boxClass: 'wow', // default
+  animateClass: 'animated', // default
+  offset: 0, // default
+  mobile: true, // default
+  live: true // default
+})
+wow.init();

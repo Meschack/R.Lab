a0d47c99cd5983ad969ab7d2373ebe50b30aa888
Irung <47919550+Meschack@users.noreply.github.com>
2019-12-31 18:29:15 +0200

    Add files via upload

--- R.Lab/js/main.js
+++ R.Lab/js/main.js
@@ -1,24 +1,42 @@
-$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
+var preload = document.getElementById('loading');
 
-  for (var i=0;i<3;i++) {
-    next=next.next();
-    if (!next.length) {
-      next = $(this).siblings(':first');
-    }
-    next.children(':first-child').clone().appendTo($(this));
-  }
+function myFunction()
+{
+    preload.style.delay = '100000';
+    preload.style.display = 'inheret';
+}
+
+// popovers initialization - on hover
+$('[data-toggle="popover-hover"]').popover({
+  html: true,
+  trigger: 'hover',
+  placement: 'bottom',
+  content: function () { return '<img src="' + $(this).data('img') + '" />'; }
 });
 
-wow = new WOW({
-  boxClass: 'wow', // default
-  animateClass: 'animated', // default
-  offset: 0, // default
-  mobile: true, // default
-  live: true // default
-})
-wow.init();
+var lab = document.querySelector('#active');
+function t1(){
+  lab.src='images/row/Clay-iPad-9.7-Mockup.jpg';
+}
+
+function t2(){
+  lab.src='images/row/mockup ipad.jpg';
+}
+
+function t3(){
+  lab.src='images/row/Free-iPad-Mockup-Top-View-1000x750.jpg';
+}
+
+function t4(){
+  lab.src='images/row/mockup ipad.jpg';
+}
+
+var btnt1 = document.querySelector('#t1');
+var btnt2 = document.querySelector('#t2');
+var btnt3 = document.querySelector('#t3');
+var btnt4 = document.querySelector('#t4');
+
+btnt1.addEventListener('click', t1);
+btnt2.addEventListener('click', t2);
+btnt3.addEventListener('click', t3);
+btnt4.addEventListener('click', t4);
